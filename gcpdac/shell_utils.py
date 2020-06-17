947334b4252b16d3dbfe7fbd9a552c340083aca5
snul <simon.underhill@gft.com>
2020-06-17 08:15:30 +0100

    update jenkins url

--- gcpdac/shell_utils.py
+++ gcpdac/shell_utils.py
@@ -33,7 +33,7 @@ def call_jenkins(git_repo_url):
     # git_repo_url=git_repo_url,
     # jenkins_server=jenkins_server)
     # TODO this is a hard-coded jenkins server! just for demo, remove after
-    call_string = "curl -X POST http://remote_user:11eccd7308d0e2f408e53f04b16c839e65@34.89.62.54:8080/job/Activator%20Deploy/build?token=11eccd7308d0e2f408e53f04b16c839e65"
+    call_string = "curl -X POST http://remote_user:11eccd7308d0e2f408e53f04b16c839e65@34.105.172.132:8080/job/Activator%20Deploy/build?token=11eccd7308d0e2f408e53f04b16c839e65"
     call_process(call_string)
 
 
